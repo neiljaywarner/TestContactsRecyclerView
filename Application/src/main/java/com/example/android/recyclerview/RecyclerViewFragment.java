941a79e2d601b3537884b07fa679caeffca89287
nwarner <nwarner@esi-estech.com>
2015-06-17 18:03:12 -0500

    actoin_call_button is up/down handset

--- Application/src/main/java/com/example/android/recyclerview/RecyclerViewFragment.java
+++ Application/src/main/java/com/example/android/recyclerview/RecyclerViewFragment.java
@@ -16,7 +16,10 @@
 
 package com.example.android.recyclerview;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -26,16 +29,77 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
 
+import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.Contacts.Photo;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+
+import com.example.android.common.logger.Log;
+
 /**
  * Demonstrates the use of {@link RecyclerView} with a {@link LinearLayoutManager} and a
  * {@link GridLayoutManager}.
  */
-public class RecyclerViewFragment extends Fragment {
+public class RecyclerViewFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = "RecyclerViewFragment";
     private static final String KEY_LAYOUT_MANAGER = "layoutManager";
     private static final int SPAN_COUNT = 2;
     private static final int DATASET_COUNT = 60;
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        // If this is the loader for finding contacts in the Contacts Provider
+        // (the only one supported)
+        if (id == ContactsQuery.QUERY_ID) {
+            Uri contentUri;
+
+            // There are two types of searches, one which displays all contacts and
+            // one which filters contacts by a search query. If mSearchTerm is set
+            // then a search query has been entered and the latter should be used.
+
+
+                contentUri = ContactsQuery.CONTENT_URI;
+
+
+            // Returns a new CursorLoader for querying the Contacts table. No arguments are used
+            // for the selection clause. The search string is either encoded onto the content URI,
+            // or no contacts search string is used. The other search criteria are constants. See
+            // the ContactsQuery interface.
+            return new CursorLoader(getActivity(),
+                    contentUri,
+                    ContactsQuery.PROJECTION,
+                    ContactsQuery.SELECTION,HN
+                    null,
+                    ContactsQuery.SORT_ORDER);
+        }
+
+        Log.e(TAG, "onCreateLoader - incorrect ID provided (" + id + ")");
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // This swaps the new cursor into the adapter.
+        if (loader.getId() == ContactsQuery.QUERY_ID) {
+          //  mAdapter.swapCursor(data);
+            data.
+
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        if (loader.getId() == ContactsQuery.QUERY_ID) {
+            // When the loader is being reset, clear the cursor from the adapter. This allows the
+            // cursor resources to be freed.
+           // mAdapter.swapCursor(null);
+        }
+    }
 
     private enum LayoutManagerType {
         GRID_LAYOUT_MANAGER,
@@ -59,6 +123,14 @@ public class RecyclerViewFragment extends Fragment {
         // Initialize dataset, this data would usually come from a local content provider or
         // remote server.
         initDataset();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(ContactsQuery.QUERY_ID, null, this);
+
+
     }
 
     @Override
@@ -157,4 +229,72 @@ public class RecyclerViewFragment extends Fragment {
             mDataset[i] = "This is element #" + i;
         }
     }
+
+    public static boolean hasHoneyComb() {
+        return true;
+    }
+    // see http://grepcode.com/file_/repository.grepcode.com/java/ext/com.google.android/android-apps/4.4_r1/com/example/android/contactslist/ui/ContactsListFragment.java/?v=source
+    /**
+     * This interface defines constants for the Cursor and CursorLoader, based on constants defined
+     * in the {@link android.provider.ContactsContract.Contacts} class.
+     */
+    public interface ContactsQuery {
+
+        // An identifier for the loader
+        final static int QUERY_ID = 1;
+
+        // A content URI for the Contacts table
+        final static Uri CONTENT_URI = Contacts.CONTENT_URI;
+
+        // The search/filter query Uri
+        final static Uri FILTER_URI = Contacts.CONTENT_FILTER_URI;
+
+        // The selection clause for the CursorLoader query. The search criteria defined here
+        // restrict results to contacts that have a display name and are linked to visible groups.
+        // Notice that the search on the string provided by the user is implemented by appending
+        // the search string to CONTENT_FILTER_URI.
+        final static String SELECTION =
+                (hasHoneyComb() ? Contacts.DISPLAY_NAME_PRIMARY : Contacts.DISPLAY_NAME) +
+                        "<>''" + " AND " + Contacts.IN_VISIBLE_GROUP + "=1";
+
+
+        final static String SORT_ORDER =
+                hasHoneyComb() ? Contacts.SORT_KEY_PRIMARY : Contacts.DISPLAY_NAME;
+
+
+        final static String[] PROJECTION = {
+
+                // The contact's row id
+                ContactsContract.Contacts._ID,
+
+                // A pointer to the contact that is guaranteed to be more permanent than _ID. Given
+                // a contact's current _ID value and LOOKUP_KEY, the Contacts Provider can generate
+                // a "permanent" contact URI.
+                Contacts.LOOKUP_KEY,
+
+                // In platform version 3.0 and later, the Contacts table contains
+                // DISPLAY_NAME_PRIMARY, which either contains the contact's displayable name or
+                // some other useful identifier such as an email address. This column isn't
+                // available in earlier versions of Android, so you must use Contacts.DISPLAY_NAME
+                // instead.
+                hasHoneyComb() ? Contacts.DISPLAY_NAME_PRIMARY,
+
+                // In Android 3.0 and later, the thumbnail image is pointed to by
+                // PHOTO_THUMBNAIL_URI. In earlier versions, there is no direct pointer; instead,
+                // you generate the pointer from the contact's ID value and constants defined in
+                // android.provider.ContactsContract.Contacts.
+                hasHoneyComb() ? Contacts.PHOTO_THUMBNAIL_URI,
+
+                // The sort order column for the returned Cursor, used by the AlphabetIndexer
+                SORT_ORDER,
+        };
+
+        // The query column numbers which map to each value in the projection
+        final static int ID = 0;
+        final static int LOOKUP_KEY = 1;
+        final static int DISPLAY_NAME = 2;
+        final static int PHOTO_THUMBNAIL_DATA = 3;
+        final static int SORT_KEY = 4;
+    }
+
 }
